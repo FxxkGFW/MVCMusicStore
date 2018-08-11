2234949fc18a91e6c2f56b4ec90381ef9c82d7bb
ChenQiao <twins-cq@163.com>
2018-08-11 09:17:53 +0800

    添加项目文件。

---
+++ MVCMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Asp.NET MVC5开发的音乐商店";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
